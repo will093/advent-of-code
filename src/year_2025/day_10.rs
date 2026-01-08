b9bba1d05c7fc47b0e811b310f36381def2744d9
Will Taylor <willtaylor.developer@gmail.com>
2026-01-08 17:34:10 +0000

    attempt at improved d10part2

--- src/year_2025/day_10.rs
+++ src/year_2025/day_10.rs
@@ -1,6 +1,4 @@
-use std::env::var;
-use std::iter::zip;
-
+use std::collections::{BTreeSet};
 use crate::utils::solver::Solver;
 use crate::utils::parse::{AocParseExt};
 use itertools::Itertools;
@@ -29,7 +27,6 @@ impl Solver for Day10Part2 {
 }
 
 type Machine = (usize, Vec<usize>, Vec<i32>);
-type Input = (Vec<i32>, Vec<i32>);
 
 fn parse(input: &str) -> Vec<Machine> {
     input
@@ -90,18 +87,20 @@ fn configure_lights((lights, buttons, _): &Machine) -> u32 {
 fn solve_two(machines: &mut Vec<Machine>) -> String {
     machines
         .iter()
-        .map(|m| configure_joltage(m))
-        .sum::<u32>()
+        .enumerate()
+        .map(|(i, m)| { 
+            println!("machine {}", i);
+            configure_joltage(m) 
+        })
+        .sum::<i32>()
         .to_string()
 } 
 
-
-fn configure_joltage((_, buttons, joltages): &Machine) -> u32 {
-    
-    // Step 1: Build a system of linear equations from the buttons and joltages
+fn build_linear_system((_, buttons, joltages): &Machine) -> (usize, usize, Vec<i32>, Vec<Vec<i32>>) {
     let width = buttons.len() + 1;
     let height = joltages.len();
 
+    
     let mut equations: Vec<Vec<i32>> = vec![vec![0; width]; height];
 
     for ((i, eq), joltage) in equations.iter_mut().enumerate().zip(joltages) {
@@ -112,107 +111,149 @@ fn configure_joltage((_, buttons, joltages): &Machine) -> u32 {
                 eq[j] = 1;
             }
         }
-
     }
 
+    let mut maximums: Vec<i32> = vec![0; width - 1];
+    for i in 0..width-1 {
+        maximums[i] = equations.iter().filter(|eq| eq[i] != 0).map(|eq| eq[eq.len()-1] / eq[i]).min().unwrap();
+    }
 
-    println!("original");
-    print_matrix(&equations);
+    (width, height, maximums, equations)
+}
 
-    // Step 2: convert to row echelon form using Gaussian elimination
-    let mut pivot_row = 0;
-    'outer: for col in 0..(width-1) {
-        // Find the first row where col is not zero and can be reduced to 1.
-        while let Some((row_index, _)) = equations[pivot_row..].iter().find_position(|&eq| eq[col] != 0 && eq.iter().all(|v| v % eq[col] == 0)) {
-            let pivot_col = col;
-            // Put the pivot row into the right position for row echelon form.
-            equations.swap(pivot_row, row_index + pivot_row);
+fn reduce_linear_system(width: usize, height: usize, equations: &mut Vec<Vec<i32>>) -> (BTreeSet<usize>,  &mut Vec<Vec<i32>>) {
+    let mut current_free_vars: BTreeSet<_> = (0..width-1).collect();
+    let mut prev_free_vars: BTreeSet<usize> = BTreeSet::new();
 
-            let (pivot_equation, remaining_equations) = equations[pivot_row..].split_at_mut(1);
-            
-            // Divide each col in pivot row by pivot col (make pivot col equal to 1).
-            pivot_equation[0] = pivot_equation[0].iter().map(|col| col / pivot_equation[0][pivot_col]).collect();
+    while current_free_vars != prev_free_vars {
+        let mut pivot_row = 0;
+        let mut pivot_col = 0;
 
+        prev_free_vars = current_free_vars;
+        current_free_vars = (0..width).collect();
+    
+        while pivot_row < height && pivot_col < width - 1 {
+            let Some((selected, _)) = equations[pivot_row..]
+                .iter()
+                .find_position(|eq| eq[pivot_col] != 0 && eq.iter().all(|val| (val % eq[pivot_col]) == 0)) else {
+                    pivot_col += 1;
+                    continue;
+                };
 
-            for eq in remaining_equations.iter_mut() {
-                let multiple = eq[pivot_col];
-                for i in 0..eq.len() {
-                    eq[i] = eq[i] - (multiple * pivot_equation[0][i]);
+            // Move the pivot row to its required location above all other pivot rows.
+            equations.swap(pivot_row, selected + pivot_row);
+
+            // Scale the pivot row so pivot col equals 1
+            equations[pivot_row] = equations[pivot_row].iter().map(|val| val/equations[pivot_row][pivot_col]).collect();
+
+            let pivot_equation = equations[pivot_row].clone();
+
+            for (i, eq) in equations.iter_mut().enumerate() {
+                if i != pivot_row {
+                    let multiple = eq[pivot_col];
+
+                    for j in 0..eq.len() {
+                        eq[j] -= multiple * pivot_equation[j];
+                    }
                 }
             }
 
             pivot_row += 1;
-            if pivot_row >= height {
-                break 'outer;
-            }
-        }
+            pivot_col += 1;
+
+            current_free_vars.remove(&pivot_col);
+        }        
     }
 
-    println!("row echelon");
-    print_matrix(&equations);
-
-    // Step 3: Brute force the free variables
-    let variables: Vec<Option<i32>> = vec![None; width - 1];
-    let solutions = get_solutions(&variables, &equations);
-
-    // println!("Solutions");
-    // dbg!(&solutions);
-
-    // Approach - give each variable a min and max, generate these combinations, calculate which solve the problem 
-    // and then return the minimum of these.
-
-
-    solutions.len() as u32
+    (current_free_vars, equations)
 }
 
-fn get_solutions(variables: &Vec<Option<i32>>, equations: &Vec<Vec<i32>>) -> Vec<Vec<Option<i32>>> {
-    // println!("vars {:?}", variables);
-    for eq in equations.iter().rev() {
+fn configure_joltage(machine: &Machine) -> i32 {
+    
+    // Step 1: Build a system of linear equations from the buttons and joltages
+    let (width, height, maximums, mut equations) = build_linear_system(machine);
 
-        let rhs = *eq.iter().last().expect("equation should have variables");
-        let lhs = &eq[..(eq.len()-1)];
+    println!("finished step 1");
+    print_matrix(&equations);
 
-        // Find the first free variable if there is one
-        let free_var = lhs.iter().zip(variables).find_position(|&(coeff, var)| {
-            *coeff != 0 && *var == None
-        });
+    // Step 2 - reduce to row echelon form using Gaussian elimination
+    // Row echelon form - we want to iterate through the cols and for each col find a pivot row whose value before the pivot should equal 0.
+    // Ideally we want to reduce the pivot col to 1 to make the rest of the algorithm simpler.
+    // We may need to iterate multiple times to achieve row echelon form in some cases.
 
-        // println!("free var {:?} {} {:?}", &free_var, rhs, eq);
-        match free_var {
-            // No free variables - check if it is a correct solution.
-            None => {
-                // println!("solution found {:?} {:?}", variables, eq);
-                let sum_lhs = lhs.iter()
-                    .zip(variables)
-                    .fold(0,|acc, (coeff, var)| {
-                        if *coeff == 0 {
-                            return acc;
-                        } else {
-                            return var.expect("already established no free vars with non-zero coefficient") * coeff + acc
-                        }
-                    });
-                if rhs == sum_lhs {
-                    continue;
-                } else {
-                    return vec![];
-                }
-            },
-            // Has a free variable - so we try fixing it to each possible value
-            Some((index, _)) => {
-                return (0..=40).flat_map(|value| {
-                    let mut try_variables = variables.clone();
-                    try_variables[index] = Some(value);
-                    // println!("Vars to try");
-                    // dbg!(&try_variables);
-                    // get_solutions(&try_variables, equations)
-                    return vec![];
-                }).collect()
-            },
-            Some(_) => panic!("Should not reach this")
+
+    let (free_var_cols, equations) = reduce_linear_system(width, height, &mut equations);
+
+    println!("finished step 2");
+    print_matrix(&equations);
+
+    // First imagine that all free vars are 0.
+    // Sum all fixed cols and this is the presses from fixed vars
+    // for each free var, free search a value between 0 and its max
+    // subtract coefficient * val from the RHS of each equation
+    // we *should* end up with a solution where RHS is 0 for rows with no fixed vars and positive for rows with fixed vars
+
+    let pivot_row_count = width - 1 - free_var_cols.len();
+    let presses = equations[..pivot_row_count].iter().flat_map(|v| v.iter().last()).sum::<i32>();
+
+    let mut free_var_coefficients: Vec<Vec<i32>> = vec![vec![0; height]; free_var_cols.len()];
+    let mut cost: Vec<i32> = vec![0; width - free_var_cols.len()];
+
+    for i in 0..free_var_cols.len() {
+        cost[i] = 1 - equations[..pivot_row_count].iter().map(|eq| eq[pivot_row_count + i]).sum::<i32>();
+        for j in 0..height {
+            free_var_coefficients[i][j] = equations[j][i+pivot_row_count]
         }
     }
 
-    vec![variables.clone()]
+    let mut rhs: Vec<Vec<i32>> = vec![vec![0; height]; free_var_cols.len() + 1];
+    rhs[0] = equations
+        .iter()
+        .map(|eq| *eq.iter().last().unwrap())
+        .collect();
+
+    // Step 3: Brute force the free variables
+
+
+    solve_reduced_equations(presses, &free_var_coefficients, &mut rhs, 0, pivot_row_count, &maximums, &cost).unwrap()
+}
+
+fn solve_reduced_equations(presses: i32, free_var_coefficients: &Vec<Vec<i32>>, rhs: &mut Vec<Vec<i32>>, depth: usize, pivot_row_count: usize, maximums: &Vec<i32>, cost: &Vec<i32>) -> Option<i32> {
+    // println!("- - - - - Solving - - - - -");
+    // println!("presses {}", presses);
+    // println!("free var coefficients");
+    // print_matrix(free_var_coefficients);
+    // println!("rhs");
+    // print_matrix(rhs);
+    // println!("depth {}", depth);
+    // println!("pivot row count {}", pivot_row_count);
+    // println!("maximums {:?}", maximums);
+    // println!("cost {:?}", cost);
+    
+    if depth == free_var_coefficients.len() {
+        let pivot_rows_positive = rhs[depth].iter().take(pivot_row_count).all(|&val| val > 0);
+        let non_pivot_rows_zero = rhs[depth].iter().skip(pivot_row_count).all(|&val| val == 0);
+        if pivot_rows_positive && non_pivot_rows_zero {
+            return Some(presses);
+        } else {
+            // println!("depth {:?} {}", rhs[depth], pivot_row_count);
+            return None;
+        }
+    } else {
+        (0..maximums[depth])
+            .filter_map(|x| {
+                let updated_presses = presses + (x * cost[depth]);
+
+                for row in 0..rhs[0].len() {
+                    // print_matrix(&rhs);
+                    rhs[depth+1][row] = rhs[depth][row] - (x * free_var_coefficients[depth][row]);
+                }
+
+                solve_reduced_equations(updated_presses, free_var_coefficients, rhs, depth+1, pivot_row_count, maximums, cost)
+            })
+            .min()
+    }
+
 }
 
 // TODO: move to helper
